5525fca4c7b30441fa81f847569d12094c29b2dc
Seungwoon <swdev678@gmail.com>
2021-12-22 22:16:00 +0900

    jpql 중급 학습 및 jpa 기본 마무리

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -15,31 +15,41 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
             Member member1 = new Member();
-            member1.setUsername("관리자1");
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
 
-            em.flush();
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
+
+//            em.flush();
+//            em.clear();
+
+            //flush 자동 호출
+            int resultCount = em.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
+
             em.clear();
 
-            String query = "select group_concat(m.username) from Member m";
+            Member findMember = em.find(Member.class, member1.getId());
 
-            List<String> result = em.createQuery(query, String.class)
-                    .getResultList();
-
-            for (String s : result) {
-                System.out.println("s = " + s);
-            }
-
-         
+            System.out.println("findMember = " + findMember.getAge());
 
 
             tx.commit();
